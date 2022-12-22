d82018b7706da8a40e47ccc3ae0f0a5bd3b1bb7d
hugistaken <102199762+hugistaken@users.noreply.github.com>
2022-12-22 15:08:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    server = run_server()
+if __name__ == "__main__": 
     # debug True reruns web app everytime we change code
     app.run(debug=True)
