0e36e90e90e46fde09f64830bae74831d6c623f0
hugistaken <102199762+hugistaken@users.noreply.github.com>
2022-12-22 19:11:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# Import the create_app function from the website folder
 from website import create_app
 
 app = create_app()
 
+# Check to see if script is being run directly or as a module. If directly, then run 
 if __name__ == "__main__": 
-    # debug True reruns web app everytime we change code
+    # Start the server and run app, debug true means changes in code are reflected in the app in real time. 
     app.run(debug=True)
